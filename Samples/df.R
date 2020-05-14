91adfedd461a503b2375a6919b04ab977dda8cf9
Akhilagp <akhilapadmanaban@gmail.com>
2020-05-14 18:34:21 +0530

    R programming

---
+++ Samples/df.R
@@ -0,0 +1,2 @@
+structure(list(f = 0:3, b = c(FALSE, TRUE, TRUE, TRUE)), .Names = c("f", 
+"b"), row.names = c(NA, -4L), class = "data.frame")
---
+++ Samples/df1.R
@@ -0,0 +1,10 @@
+tfinal <-
+structure(list(V1 = 1:10, V2 = structure(c(2L, 1L, 10L, 6L, 5L, 
+3L, 9L, 7L, 4L, 8L), .Label = c("Aa", "Ak", "G", 
+"J", "Ke", "Padman", "P", "Pr", 
+"Sa", "S"), class = "factor"), V3 = c(21, 20, 49, 
+56, 20, 21, 21, 21, 21, 21)), .Names = c("V1", "V2", "V3"), class = "data.frame", row.names = c(NA, 
+-10L))
+df <-
+structure(list(f = 0:3, b = c(FALSE, TRUE, TRUE, TRUE)), .Names = c("f", 
+"b"), row.names = c(NA, -4L), class = "data.frame")
